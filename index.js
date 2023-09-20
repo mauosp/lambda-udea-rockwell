4f23d858676e5f705cce8521c1cb854acb601379
mauosp <55215654+mauosp@users.noreply.github.com>
2023-09-20 15:22:17 +0000

    Initial commit from lambda

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Loading function');
+
+exports.handler = async (event, context) => {
+    console.log('Received event:', JSON.stringify(event, null, 2));
+    console.log('Community Info: ', event.message);
+    return "Hello People from the "+event.message;
+    // throw new Error('Something went wrong');
+};
