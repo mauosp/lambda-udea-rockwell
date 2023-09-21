81fff833d8de96ba5db4a525a93462409eb1ec0d
mcMEJIA <mejiasebastian1499@gmail.com>
2023-09-21 13:33:02 -0500

    Change message

--- HelloUdeALambda/index.js
+++ HelloUdeALambda/index.js
@@ -4,6 +4,6 @@ exports.handler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
     console.log('Community Info: ', event.message);
     console.log('Console for test #2');
-    return "Hello People From The "+event.message;
+    return "Hello People From The UdeA";
     // throw new Error('Something went wrong');
 };
